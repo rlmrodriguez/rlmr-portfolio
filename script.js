78dc2bdb0ea79a75ec26b0255f3bf3bc39c480b2
RLMR <roden.mrodriguez@gmail.com>
2021-05-21 18:08:25 +0800

    Disable nav/sidebar and edit styles for responsiveness

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const sidebarBtn = document.getElementById('sidebarBtn');
 const navbar = document.getElementById('navbar');
 
-sidebarBtn.addEventListener('click', () => {
-  sidebarBtn.classList.toggle('active');
-  if (sidebarBtn.classList.contains('active')) {
-    navbar.style.right = '0';
-  } else {
-    navbar.style.right = '-60%';
-  }
-});
+// sidebarBtn.addEventListener('click', () => {
+//   sidebarBtn.classList.toggle('active');
+//   if (sidebarBtn.classList.contains('active')) {
+//     navbar.style.right = '0';
+//   } else {
+//     navbar.style.right = '-60%';
+//   }
+// });
