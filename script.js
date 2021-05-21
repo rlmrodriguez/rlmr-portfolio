e1b9e9671b101ae69fa838e6ffbc43cbde9cd478
RLMR <roden.mrodriguez@gmail.com>
2021-05-21 19:17:29 +0800

    Fix nav/sidebar issues and logo position

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const sidebarBtn = document.getElementById('sidebarBtn');
 const navbar = document.getElementById('navbar');
+const sidebar = document.getElementById('sidebar');
 
-// sidebarBtn.addEventListener('click', () => {
-//   sidebarBtn.classList.toggle('active');
-//   if (sidebarBtn.classList.contains('active')) {
-//     navbar.style.right = '0';
-//   } else {
-//     navbar.style.right = '-60%';
-//   }
-// });
+sidebarBtn.addEventListener('click', () => {
+  sidebarBtn.classList.toggle('active');
+  if (sidebarBtn.classList.contains('active')) {
+    sidebar.style.right = '0';
+  } else {
+    sidebar.style.right = '-250px';
+  }
+});
