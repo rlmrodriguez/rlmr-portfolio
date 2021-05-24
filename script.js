07ca6557cc1fd1741abb595a497cba7b6be43ae6
RLMR <roden.mrodriguez@gmail.com>
2021-05-24 15:01:25 +0800

    Add js click event

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const sidebarBtn = document.getElementById('sidebarBtn');
 const navbar = document.getElementById('navbar');
 const sidebar = document.getElementById('sidebar');
 
-sidebarBtn.addEventListener('click', () => {
+sidebarBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
   sidebarBtn.classList.toggle('active');
-  if (sidebarBtn.classList.contains('active')) {
-    sidebar.style.right = '0';
-  } else {
-    sidebar.style.right = '-250px';
+  sidebar.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target.id !== 'sidebarBtn' && e.target.id !== 'sidebar') {
+    sidebarBtn.classList.remove('active');
+    sidebar.classList.remove('active');
   }
+  console.log(e.target);
 });
