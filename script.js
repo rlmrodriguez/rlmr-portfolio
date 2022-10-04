a9f02fa19bf95e9e95623828d97bec69bc8cc673
kulasphere <roden.mrodriguez@gmail.com>
2022-10-05 00:20:02 +0800

    Update files and assets

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const sidebarBtn = document.getElementById('sidebarBtn');
-const navbar = document.getElementById('navbar');
-const sidebar = document.getElementById('sidebar');
+const sidebarBtn = document.querySelector("#sidebarBtn");
+const navbar = document.querySelector("#navbar");
+const sidebar = document.querySelector("#sidebar");
+const hiddenEls = document.querySelectorAll(".hidden");
 
-sidebarBtn.addEventListener('click', (e) => {
-  e.stopPropagation();
-  sidebarBtn.classList.toggle('active');
-  sidebar.classList.toggle('active');
-});
-
-document.addEventListener('click', (e) => {
-  if (e.target.id !== 'sidebarBtn' && e.target.id !== 'sidebar') {
-    sidebarBtn.classList.remove('active');
-    sidebar.classList.remove('active');
+const allowScroll = () => {
+  if (sidebar.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
   }
-  console.log(e.target);
+};
+
+sidebarBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sidebarBtn.classList.toggle("active");
+  sidebar.classList.toggle("active");
+
+  allowScroll();
 });
+
+document.addEventListener("click", (e) => {
+  if (e.target.id !== "sidebarBtn" && e.target.id !== "sidebar") {
+    sidebarBtn.classList.remove("active");
+    sidebar.classList.remove("active");
+  }
+  allowScroll();
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting);
+    if (entry.isIntersecting) observer.unobserve(entry.target);
+  });
+});
+
+hiddenEls.forEach((el) => observer.observe(el));
